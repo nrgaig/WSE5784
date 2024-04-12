56f9d6c853790dabcc73da0831bc7001f7a4ea28
zivf23 <zfarajun@g.jct.ac.il>
2024-04-12 11:11:02 +0300

    accuracy of the classes for the MVP architecture, need to connect the model to the database and implement the methods, and integrate the main window with the VIEW

--- frontHand/stock.py
+++ frontHand/stock.py
@@ -1,7 +1,20 @@
 #model
 class Stock: 
+    __tablename__ = 'stocks'
     def __init__ (self, symbol, company_name, current_price):
         self.symbol = symbol
         self.company_name = company_name
         self.current_price = current_price
 #TODO conect to the database
+
+    def delete_stock(symbol):
+        pass
+    def add_stock(symbol, company_name, price):
+        pass
+    def get_all_stocks():
+        pass
+    def get_stock(symbol):
+        pass
+    def get_stock_by_company_name(company_name):
+        pass
+        
