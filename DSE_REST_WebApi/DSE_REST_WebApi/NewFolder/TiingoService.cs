7136f3cf97ad7de59220c9a112bc2e82cf1f229d
MrFrost <89649789+nrgaig@users.noreply.github.com>
2024-04-17 15:54:46 -0700

    fix tiingo get not working

--- DSE_REST_WebApi/DSE_REST_WebApi/NewFolder/TiingoService.cs
+++ DSE_REST_WebApi/DSE_REST_WebApi/NewFolder/TiingoService.cs
@@ -5,13 +5,13 @@ namespace WSE_REST_WebApi.NewFolder
 {
     public class TiingoService
     {
-        private readonly static HttpClient _httpClient;
+        //private readonly static HttpClient _httpClient;
         private readonly static string frosToken = "78e2a84e3c3fd84749a6d9b171689dc4a08e8f7b";
         private static readonly string  BaseUrl = "https://api.tiingo.com/tiingo/";
 
         public static async Task<ActionResult<Stream>> GetStockId(string id)
         {
-            using (HttpClient httpClient = new HttpClient())
+            using (HttpClient _httpClient = new HttpClient())
             {
                 var query = $"{BaseUrl}daily/{id}?token={frosToken}";
                 var response = await _httpClient.GetAsync(query);
