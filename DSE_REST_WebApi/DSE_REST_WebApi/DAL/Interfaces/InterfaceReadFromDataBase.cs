8a2bd376e9f870b56e45b1ca1863f2d4c3092872
OmerAvidar <oavidar@g.jct.ac.il>
2024-05-09 21:04:07 +0300

    Adding Interface Database

---
+++ DSE_REST_WebApi/DSE_REST_WebApi/DAL/Interfaces/InterfaceReadFromDataBase.cs
@@ -0,0 +1,19 @@
+﻿using WSE_REST_WebApi.Models;
+
+namespace WSE_REST_WebApi.DAL.Interfaces
+{
+    public interface InterfaceReadFromDataBase
+    {
+      
+        //Stocks
+        Task<IEnumerable<Stock>> GetStocksAsync();
+        Task<Stock?> GetStockByTiingoStringAsync(string ticker);
+        Task<Stock?> GetStockByIdAsync(int id);
+
+
+        //TiingoPriceDTO
+        Task<List<TiingoPriceDto?>> GetPricesByTiingoTickerAsync(string ticker);
+        Task<List<TiingoPriceDto?>> GetPricesByIdAsync(int id);
+
+    }
+}
---
+++ DSE_REST_WebApi/DSE_REST_WebApi/DAL/Interfaces/InterfaceWriteToDatabase.cs
@@ -0,0 +1,22 @@
+﻿using WSE_REST_WebApi.Models;
+namespace WSE_REST_WebApi.DAL.Interfaces
+{
+    public interface InterfaceWriteToDatabase
+    {
+
+        //Stocks
+        Task<Stock> AddMStockAsync(Stock stock);
+        Task<Stock> UpdateStockAsync(Stock stock);
+        Task DeleteStockAsync(Stock stock);
+
+
+        //TiingoPriceDto
+        //לבדוק מה בדיוק לעשות כאן מבחינת הקוד והיצירה האם צריך גם את המניה
+
+        Task<TiingoPriceDto> AddTiingoPriceDtoAsync(Stock stock, TiingoPriceDto tiingoPriceDto);
+        Task<TiingoPriceDto> UpdateTiingoPriceDtoAsync(Stock stock, TiingoPriceDto tiingoPriceDto);
+        Task DeleteTiingoPriceDto(TiingoPriceDto tiingoPriceDto);
+
+
+    }
+}
